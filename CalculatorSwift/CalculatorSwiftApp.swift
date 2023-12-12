541055df64150ad7c1f696a39a18feac05fcc244
laibaoguang <laibaoguang@meituan.com>
2023-12-12 15:35:40 +0800

    Initial Commit

---
+++ CalculatorSwift/CalculatorSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftApp.swift
+//  CalculatorSwift
+//
+//  Created by 赖宝光 on 2023/12/12.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
